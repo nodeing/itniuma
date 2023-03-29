2a0f668c2573a3e9b3821f1d4da79c4e27113a37
key <key@keydeMacBook-Air.local>
2023-03-30 00:30:13 +0800

    添加about

--- docs/.vitepress/config/nav.js
+++ docs/.vitepress/config/nav.js
@@ -2,5 +2,6 @@ export default [
   { text: '首页', link: '/' },
   { text: '路线', link: '/learn-path/' },
   { text: '服务', link: '/service/' },
-  { text: '文章', link: '/article/' }
+  { text: '文章', link: '/article/' },
+  { text: '关于', link: '/article/about/' }
 ]
