c8fb67c5412cdae4c47edd759f304fc9140b99b7
key <key@keydeMacBook-Air.local>
2023-03-29 13:14:16 +0800

    修改build命令

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,7 +3,7 @@ import sidebar from './config/siderbar'
 export default {
   appearance: 'white',
   title: '洛邪',
-  base: '/itboma/',
+  base: 'itboma',
   description: '洛邪的编程笔记｜web3教程｜web3开发｜区块链｜智能合约｜dapp开发',
   head: [
     [
