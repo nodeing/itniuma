20beb48c9b59abc7a6199c259f0520c5a19de6fe
nodeing <cnode@sina.com>
2023-05-28 22:50:11 +0800

    gpt使用指南修复文章链接

--- docs/.vitepress/config/nav.js
+++ docs/.vitepress/config/nav.js
@@ -2,6 +2,6 @@ export default [
   { text: '首页', link: '/' },
   { text: '路线', link: '/learn-path/' },
   { text: '服务', link: '/service/' },
-  { text: 'AI应用', link: '/article/ai' },
+  { text: 'AI应用', link: '/article/ai/' },
   { text: '关于', link: '/article/about/' }
 ]
---
+++ docs/article/ai/index.md
@@ -0,0 +1,3 @@
+# 文章归档
+
+[国内稳定版 GPT 使用指南](/article/ai/chatgpt/1.html)
