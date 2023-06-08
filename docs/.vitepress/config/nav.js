b259b3816d83597f6324d2c1352811162ea12748
chatgptdemo888 <luoxie@proton.me>
2023-06-08 16:53:13 +0800

    添加文章-屎山代码规范

--- docs/.vitepress/config/nav.js
+++ docs/.vitepress/config/nav.js
@@ -3,5 +3,6 @@ export default [
   { text: '路线', link: '/learn-path/' },
   { text: '服务', link: '/service/' },
   { text: 'AI应用', link: '/article/ai/' },
+  { text: '文章', link: '/article/' },
   { text: '关于', link: '/article/about/' }
 ]
