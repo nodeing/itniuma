8afa67d9b0bae6608586b73bebbee074f1ada4b4
chatgptdemo888 <luoxie@proton.me>
2023-06-30 11:02:25 +0800

    增加导航链接-全栈更新进度

--- docs/.vitepress/config/siderbar/course/index.js
+++ docs/.vitepress/config/siderbar/course/index.js
@@ -4,6 +4,10 @@ export default {
       text: '课程导航',
       items: [
         {
+          text: '更新进度',
+          link: 'https://edu.nodeing.com'
+        },
+        {
           text: 'TS全栈开发',
           link: '/course/system/ts/'
         }
