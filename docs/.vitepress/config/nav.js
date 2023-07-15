60b54318ea562eac723935fb39a8dfff30643fcc
chatgptdemo888 <luoxie@proton.me>
2023-07-15 22:04:10 +0800

    增加课程更新日志栏目

--- docs/.vitepress/config/nav.js
+++ docs/.vitepress/config/nav.js
@@ -4,6 +4,7 @@ export default [
   { text: '增值服务', link: '/service/' },
   { text: '付费课程', link: '/course/system/ts/' },
   { text: 'AI应用', link: '/article/ai/' },
+  { text: '课程更新', link: '/article/course/' },
   { text: '文章', link: '/article/' },
   { text: '关于', link: '/article/about/' }
 ]
