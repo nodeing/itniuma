72e9eb14cb520956378d1f86e5c62a6ba7b48b79
nodeing <2014868983@qq.com>
2024-02-02 18:44:47 +0800

    更新接单平台文章-2024螺钉众包启航

--- docs/.vitepress/config/siderbar/article/index.js
+++ docs/.vitepress/config/siderbar/article/index.js
@@ -13,7 +13,8 @@ export default {
         { text: '重学Linux', link: '/article/linux/' },
         { text: '重学Java', link: '/article/java/' },
         { text: '重学Python', link: '/article/python/' },
-        { text: '英语学习笔记', link: '/article/english/' }
+        { text: '英语学习笔记', link: '/article/english/' },
+        { text: '重新数学', link: '/article/math/' }
       ]
     }
   ]
---
+++ docs/article/math/index.md
@@ -0,0 +1,7 @@
+# 程序员数学学习笔记
+
+- [高等数学](/article/math/gaoshu/)
+
+- [线性代数](/article/math/xiandai/)
+
+- [概率统计](/article/math/tongji/)
