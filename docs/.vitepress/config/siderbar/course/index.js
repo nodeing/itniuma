cd76fb86b1d37b9da208b0d6ef3557885dd75f1e
nodeing <2014868983@qq.com>
2024-06-15 07:14:05 +0800

    学习指南

--- docs/.vitepress/config/siderbar/course/index.js
+++ docs/.vitepress/config/siderbar/course/index.js
@@ -6,6 +6,10 @@ export default {
         {
           text: 'Java全栈接单训练营',
           link: '/course/system/java/'
+        },
+        {
+          text: '接单训练营上课指南',
+          link: '/course/system/java/guide.html'
         }
         // {
         //   text: 'TS全栈体系课',
