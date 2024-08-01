ccd67667f255f95a7a2855e4745e631069771d19
nodeing <2014868983@qq.com>
2024-08-02 00:40:31 +0800

    新增文章-我的故事

--- docs/.vitepress/config/nav.js
+++ docs/.vitepress/config/nav.js
@@ -5,5 +5,6 @@ export default [
   // { text: '实训平台', link: '/course/train/' },
   { text: '接单平台', link: '/article/crowdsourcing/index.html' },
   { text: '文章', link: '/article/' },
-  { text: '关于', link: '/article/about/' }
+  { text: '关于', link: '/article/about/' },
+  { text: '故事', link: '/article/about/me' }
 ]
