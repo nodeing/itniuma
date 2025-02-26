42ecc8055a7b7c8d30c3aa50e7833efbdf6412b0
nodeing <2014868983@qq.com>
2025-02-26 20:15:34 +0800

    共创文章

--- docs/.vitepress/config/nav.js
+++ docs/.vitepress/config/nav.js
@@ -1,8 +1,8 @@
 export default [
   { text: '首页', link: '/' },
   // { text: '免费路线', link: '/learn-path/' },
-  { text: '接单训练营', link: '/course/system/java/' },
-  // { text: '实训平台', link: '/course/train/' },
+  // { text: '接单训练营', link: '/course/system/java/' },
+  { text: '卖课训练营', link: '/article/co-creation/index.html' },
   { text: '接单社群', link: '/article/crowdsourcing/index.html' },
   { text: '独立产品', link: '/article/product/index.html' },
   { text: '文章', link: '/article/' },
